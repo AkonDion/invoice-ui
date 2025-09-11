b7ca8b8d4c7905326c5cf5aa70c9ce3b5a68ea06
AkonDion <jeremy@comforthub.ca>
2025-09-10 21:00:15 -0400

    Added data to helcim initialize route

--- app/api/helcim/initialize/route.ts
+++ app/api/helcim/initialize/route.ts
@@ -33,6 +33,8 @@ export async function POST(request: NextRequest) {
       paymentType: string;
       amount: number;
       currency: string;
+      customerCode: string;
+      invoiceNumber: string;
       paymentMethod: string;
       hasConvenienceFee: number;
       confirmationScreen: boolean;
@@ -48,6 +50,8 @@ export async function POST(request: NextRequest) {
       paymentType: 'purchase',
       amount: invoice.amount,
       currency: invoice.currency,
+      customerCode: invoice.customerCode,
+      invoiceNumber: invoice.invoiceNumber,
       paymentMethod: 'cc-ach',
       hasConvenienceFee: invoice.hasConvenienceFee ? 1 : 0, // Ensure it's exactly 1 or 0
       confirmationScreen: true,
