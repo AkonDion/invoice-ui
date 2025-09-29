1ed244ca679111977faf6b7e66a8e68aa503a0ef
AkonDion <jeremy@comforthub.ca>
2025-09-29 11:08:44 -0400

    refe

--- app/booking/[token]/error.tsx
+++ app/booking/[token]/error.tsx
@@ -111,3 +111,5 @@ export default function Error({ error, reset }: ErrorPageProps) {
 
 
 
+
+
--- components/workorder-card-with-refetch.tsx
+++ components/workorder-card-with-refetch.tsx
@@ -49,3 +49,5 @@ export function WorkOrderCardWithRefetch({ workOrderStatus }: WorkOrderCardWithR
 
 
 
+
+
